b15c0db40d856614dc8ee254707b7b697b4978cc
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-31 11:56:22 -0400

    convert frame to gray and blur

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ time.sleep(1) # wait one second for the camera to load
 
 while True:
     check, frame = video.read() #read first frame of video 
-    cv2.imshow('My video', frame)
+
+    #convert frames to gray scale frames 
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #blur method - provide frame, amt of blurness, and 0 
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    cv2.imshow('My video', gray_frame_gau)
 
     key = cv2.waitKey(1) #creates a keyboard key object
 
