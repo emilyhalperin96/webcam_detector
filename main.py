11c31fa2c003b23ec640eb47a300246757563ac0
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-07-31 11:48:48 -0400

    created video and quit

--- main.py
+++ main.py
@@ -3,9 +3,20 @@
 import cv2 
 import time
 
-video = cv2.VideoCapture(0) #set up camera
-check, frame = video.read() #read first frame of video 
 
-print(check)
-print(frame)
+video = cv2.VideoCapture(0) #set up camera
+time.sleep(1) # wait one second for the camera to load 
+
+while True:
+    check, frame = video.read() #read first frame of video 
+    cv2.imshow('My video', frame)
+
+    key = cv2.waitKey(1) #creates a keyboard key object
+
+    #if they press q key, break video 
+    if key == ord('q'):
+        break 
+
+video.release()
+
 
